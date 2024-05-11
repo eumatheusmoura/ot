a31af44f49d8f1a45ee6c3a74f94c452632580ca
Matheus Moura <matheusmouraa@icloud.com>
2024-05-11 19:19:01 -0300

    feat(header): add user avatar

---
+++ app/components/header/user-avatar.tsx
@@ -0,0 +1,15 @@
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import { ReactElement } from "react";
+
+export default function UserAvatar(): ReactElement {
+  return (
+    <section className="flex gap-2 items-center">
+      <Avatar className="h-8 w-8">
+        <AvatarImage src={"/images/user-image.jpeg"} alt="Avatar" />
+        <AvatarFallback>MM</AvatarFallback>
+      </Avatar>
+      {/* <span className="text-sm sm:hidden">Matheus Moura</span> */}
+      <span className="hidden sm:inline text-sm">Matheus Moura</span>
+    </section>
+  );
+}
