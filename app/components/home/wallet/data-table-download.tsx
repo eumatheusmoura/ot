a0d3f760b66c7db27135a0e19e6eeb23b0a7083e
Matheus Moura <matheusmouraa@icloud.com>
2024-05-21 00:56:03 -0300

    feat: export to csv component

---
+++ app/components/home/wallet/data-table-download.tsx
@@ -0,0 +1,35 @@
+"use client";
+
+import { type User } from "@/app/types/index";
+import { DownloadIcon } from "@radix-ui/react-icons";
+import { type Table } from "@tanstack/react-table";
+
+import { exportTableToCSV } from "@/lib/export";
+import { Button } from "@/components/ui/button";
+
+interface ExportToCSVProps {
+  table: Table<User>;
+}
+
+export function ExportToCSVButton({ table }: ExportToCSVProps) {
+  return (
+    <div className="flex items-center gap-2">
+      <Button
+        variant="outline"
+        onClick={() =>
+          exportTableToCSV(table, {
+            filename: "dados_carteiras",
+            excludeColumns: ["select", "actions"],
+          })
+        }
+      >
+        <DownloadIcon className="mr-2 size-4" aria-hidden="true" />
+        Exportar CSV
+      </Button>
+      {/**
+       * Other actions can be added here.
+       * For example, import, view, etc.
+       */}
+    </div>
+  );
+}
