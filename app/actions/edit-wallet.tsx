a6c2cee5a0740cc975efbac8b6623e4235a34fc7
Matheus Moura <matheusmouraa@icloud.com>
2024-05-27 12:13:27 -0300

    feat: add edit wallet actions

--- app/actions/edit-wallet.tsx
+++ app/actions/edit-wallet.tsx
@@ -1,5 +1,29 @@
-const EditWallet = () => {
-  return <div>oi</div>;
-};
+"use server";
+import { User } from "../types";
 
-export default EditWallet;
+export async function updateUser(
+  userId: number,
+  userData: Partial<User>
+): Promise<User[]> {
+  const baseUrl = process.env.BASE_URL;
+
+  try {
+    const req = await fetch(`${baseUrl}/api/users/${userId}`, {
+      method: "PATCH",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(userData),
+    });
+
+    if (!req.ok) {
+      throw new Error(`HTTP error! status: ${req.status}`);
+    }
+
+    const res: User[] = await req.json();
+    return res;
+  } catch (error) {
+    console.error("Erro ao atualizar usuário:", error);
+    return [];
+  }
+}
